3f8ef1c81656f0c3964e342092f771876d53751e
Mihret Tibebe <99690582+MihretTibebe@users.noreply.github.com>
2022-04-19 09:45:48 +0300

    Create db_connect.php

---
+++ config/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$server_name ="localhost";
+$user_name ="Utopia";
+$password ="12345678";
+$db="utopia";
+
+$conn= mysqli_connect($server_name,$user_name,$password,$db);
+
+//check conn connection
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+  ?>
